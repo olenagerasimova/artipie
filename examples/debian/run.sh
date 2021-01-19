b31e02ff265bfdd91b012cb4934053dfda25778f
olenagerasimova <olena.gerasimova@gmail.com>
2021-01-19 18:58:02 +0300

    #835 - try sudo

--- examples/debian/run.sh
+++ examples/debian/run.sh
@@ -16,8 +16,8 @@ sudo mv sources.list /etc/apt
 curl -i  -X PUT  --data-binary "@aglfn_1.7-3_amd64.deb" http://localhost:8080/my-debian/main/aglfn_1.7-3_amd64.deb
 
 # Update the world and install posted package
-apt-get update
-apt-get install -y aglfn
+sudo apt-get update
+sudo apt-get install -y aglfn
 
 # Stop Artipie.
 docker stop artipie
