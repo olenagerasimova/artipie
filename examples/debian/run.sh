921bdfece11e35464fa97bb85f113214f28eaf27
olenagerasimova <olena.gerasimova@gmail.com>
2021-01-19 18:40:40 +0300

    #835 - correct script

--- examples/debian/run.sh
+++ examples/debian/run.sh
@@ -8,7 +8,7 @@ docker run --name artipie -d -it -v $(pwd)/artipie.yaml:/etc/artipie/artipie.yml
 sleep 5
 
 # Add Artipie to Debian repository sources by modifying sources.list file
-cat sources.list
+touch sources.list
 echo "deb [trusted=yes] http://localhost:8080/my-debian my-debian main" >> sources.list
 mv sources.list /etc/apt
 
