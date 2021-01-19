438ec7a1cc7943993115824ad09d32c55799248f
olenagerasimova <olena.gerasimova@gmail.com>
2021-01-19 18:30:51 +0300

    #835 - proof script improved

--- examples/debian/run.sh
+++ examples/debian/run.sh
@@ -7,11 +7,17 @@ docker run --name artipie -d -it -v $(pwd)/artipie.yaml:/etc/artipie/artipie.yml
 # Wait for container to be ready for new connections.
 sleep 5
 
+# Add Artipie to Debian repository sources by modifying sources.list file
+cat sources.list
+echo "deb [trusted=yes] http://localhost:8080/my-debian my-debian main" >> sources.list
+mv sources.list /etc/apt
+
 # Post a package.
 curl -i  -X PUT  --data-binary "@aglfn_1.7-3_amd64.deb" http://localhost:8080/my-debian/main/aglfn_1.7-3_amd64.deb
 
+# Update the world and install posted package
+apt-get update
+apt-get install -y aglfn
+
 # Stop Artipie.
 docker stop artipie
-
-# Remove the network for inter-container communication.
-docker network rm artipie-rpm-demo
